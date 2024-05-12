0b15fe434ef324a02fee121bfac9d5af157c7242
Cem_Sarıdoğan <cemsrdgn@gmail.com>
2024-05-12 21:14:26 +0300

    AddRoom and ListRooms pages created  in a newly cleaned branch lab10.  With Bootstrap; The frontend part of the form that allows the user to add rooms to the database created inside the AddRoom page, the frontend of the table showing the list of rooms in the database created inside the ListRooms page.

---
+++ LabProject382/Pages/AddRoom.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class RoomHandlerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
